3045e5c0cd3748bb9ab0e324347a3dfadc178059
qazwsx5381 <qazwsx5381@gmail.com>
2023-07-25 17:06:37 +0900

    230725

--- note/p085_ML5js_PoseNet/poseNet.js
+++ note/p085_ML5js_PoseNet/poseNet.js
@@ -2,6 +2,8 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const result_label = document.getElementById("result_label");
+const button = document.getElementById("btn");
+const h1 = document.getElementById("content");
 
 navigator.mediaDevices
   .getUserMedia({ video: true, audio: false })
@@ -16,6 +18,46 @@ posenet.load().then((model) => {
     predict();
   };
 
+  function timerCap() {
+    model.estimateSinglePose(video).then((pose) => {
+      console.log(pose.keypoints);
+      const point = pose.keypoints;
+      if (
+        point[5].position.y > point[7].position.y &&
+        point[6].position.y > point[8].position.y &&
+        point[7].position.y > point[9].position.y &&
+        point[8].position.y > point[10].position.y &&
+        point[7].position.x > point[5].position.x &&
+        point[8].position.x < point[6].position.x &&
+        point[7].position.x > point[9].position.x &&
+        point[8].position.x < point[10].position.x
+      ) {
+        console.log("양손으로 동그라미를 그렸습니다.");
+        h1.innerHTML = "양손으로 동그라미를 그렸습니다.";
+      } else if (
+        point[5].position.y > point[9].position.y &&
+        point[6].position.y > point[10].position.y
+      ) {
+        console.log("양손을 들었습니다.");
+        h1.innerHTML = "양손을 들었습니다.";
+      } else if (point[5].position.y > point[9].position.y) {
+        console.log("왼손을 들었습니다.");
+        h1.innerHTML = "왼손을 들었습니다.";
+      } else if (point[6].position.y > point[10].position.y) {
+        console.log("오른손을 들었습니다.");
+        h1.innerHTML = "오른손을 들었습니다.";
+      } else {
+        h1.innerHTML = "";
+      }
+    });
+  }
+  setInterval(() => {
+    timerCap();
+  }, 5000);
+
+  button.addEventListener("click", () => {
+    timerCap();
+  });
   function predict() {
     model.estimateSinglePose(video).then((pose) => {
       canvas.width = video.width;
